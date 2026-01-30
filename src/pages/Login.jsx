268dcfe727ff47d1d2bcbd08673c52dedb3d72aa
Saurav8260 <166366494+Saurav8260@users.noreply.github.com>
2026-01-30 23:48:41 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ export default function Login() {
   const [form, setForm] = useState({
     name: "",
     password: "",
-    role: "",
+    role: "admin",
   });
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
